be7c097465670524902e6fd3d0f6b95595c2bd5a
Thomas Morini <thomas.morini@icloud.com>
2025-11-20 17:41:46 +0100

    v2

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
-import Login from "../components/Login";
+/* import Login from "../components/Login";
 
 function Index() {
   return <Login />;
 }
 
-export default Index;
+export default Index; */
+
+import Home from "../components/Home";
+
+function Index() {
+  return <Home/>;
+}
+
+export default Index; 
